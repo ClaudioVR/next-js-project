26fe9ca9e8536cd7b3bb5f2b33903ab0a7599d1a
Claudio Rescigno <42936120+ClaudioVR@users.noreply.github.com>
2022-09-25 14:39:53 +0200

    removed unused component

--- components/TheAppbar.js
+++ components/TheAppbar.js
@@ -16,7 +16,7 @@ const TheAppbar = () => {
     <AppBar position="static" elevation={0}>
       <Container>
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <AdbIcon sx={{ display: { xs: "flex" }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -24,7 +24,7 @@ const TheAppbar = () => {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: "none", md: "flex" },
+              display: { xs: "flex" },
               fontFamily: "monospace",
               fontWeight: 700,
               letterSpacing: ".3rem",
@@ -35,36 +35,6 @@ const TheAppbar = () => {
             Brukere.js
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-          </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {navLinks.map((page) => (
               <Button
@@ -81,8 +51,8 @@ const TheAppbar = () => {
               variant="h5"
               noWrap
               component="a"
-              href=""
               sx={{
+                display: { xs: "none", md: "flex" },
                 fontFamily: "monospace",
                 fontWeight: 700,
                 color: "inherit",
--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import theme from "../config/theme";
 import createEmotionCache from "../config/createEmotionCache";
 import Container from "@mui/material/Container";
-import Nav from "../components/Nav";
 import TheAppbar from "../components/TheAppbar";
 
 // Client-side cache, shared for the whole session of the user in the browser.
@@ -14,7 +13,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      {/* <Nav /> */}
       <TheAppbar />
       <CssBaseline />
       <Container sx={{ mb: 10 }}>
